8c5f1cd6e8a4d2dc91fce18578b3b7c0acd0a8d3
Luis <luis_devesa@hotmail.com>
2022-02-04 06:58:25 -0300

    Resolve full height
    Change height from 500px to auto

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Scroll = (props) => {
 	console.log(props);
 	return (
-		<div style={{ overflow: 'scroll', border: '1ps solid black', height: '500px' }}>
+		<div style={{ overflow: 'scroll', border: '1ps solid black', height: 'auto' }}>
 			{props.children}
 		</div>
 	);
